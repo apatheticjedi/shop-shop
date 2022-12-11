556d7a72dc076a58fe8e7adc8f7512d986d10c7c
Duder <thedudeabides1981@hotmail.com>
2022-12-10 17:23:16 -0800

    eslint error

---
+++ client/build/precache-manifest.84fb1fd5516ae380693a9a49dd327c52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33d44d1e1de35c2f14565bfa86bd635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82aeab0a5c09b7560841",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "cec733f26b4246ed8f2f",
+    "url": "/static/js/2.a47e473a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a47e473a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82aeab0a5c09b7560841",
+    "url": "/static/js/main.31b83c27.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.31b83c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
